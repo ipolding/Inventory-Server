09c3ef1ff81d0caa32e3646bbc06578d34b3eb89
Ian Polding <ipolding@gmail.com>
2014-06-22 17:25:06 +0100

    initial commit. Copy of WHS app

---
+++ Dropwizard-Whs-Mapper/src/main/java/uk/co/ipolding/whsmapper/WhsMapperApplication.java
@@ -0,0 +1,48 @@
+package uk.co.ipolding.whsmapper;
+
+
+
+import io.dropwizard.jdbi.DBIFactory;
+import org.skife.jdbi.v2.DBI;
+import uk.co.ipolding.whsmapper.jdbi.WhsDao;
+import uk.co.ipolding.whsmapper.resources.WhsResource;
+import io.dropwizard.Application;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+
+import static uk.co.ipolding.whsmapper.jdbi.DatabaseLoader.loadDatabase;
+
+public class WhsMapperApplication extends Application<WhsMapperConfiguration>{
+	
+	public static void main (String[] args) throws Exception {
+		new WhsMapperApplication().run(args);
+		}
+	
+	@Override
+	public void initialize(Bootstrap<WhsMapperConfiguration> bootstrap){
+//		Nothing to do yet
+        try {
+            loadDatabase();
+        } catch (Exception e) {
+            System.out.println(e.toString());
+        }
+		}
+
+
+
+    public void run(WhsMapperConfiguration configuration, Environment environment)throws ClassNotFoundException{
+
+		final DBIFactory factory = new DBIFactory();
+    	final DBI jdbi = factory.build(environment, configuration.getDataSourceFactory(), "sqlite");
+    	final WhsDao dao = jdbi.onDemand(WhsDao.class); // this creates an object that implements the Dao interface.
+        System.out.println("text from within run");
+		environment.jersey().register(new WhsResource(dao));
+        environment.jersey().register();
+
+
+
+
+	}
+	
+
+}
---
+++ Dropwizard-Whs-Mapper/src/main/java/uk/co/ipolding/whsmapper/resources/ImportingSyntax.java
@@ -0,0 +1,15 @@
+package uk.co.ipolding.whsmapper.resources;
+
+import java.nio.*;
+import static java.nio.file.Files.createDirectory;
+static import 
+
+
+/**
+ * Created by Ian on 07/06/2014.
+ */
+public class ImportingSyntax {
+
+    Files.createDirectory("C:\");
+
+}
